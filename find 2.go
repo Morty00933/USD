14f83d18db9b34e0766497f2229b1721987264cf
Morty00933 <103403437+Morty00933@users.noreply.github.com>
2022-04-24 03:41:19 +0300

    usd

--- find 2.go
+++ find 2.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	resp, err := http.Get("https://example.com/")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -16,5 +18,9 @@ func main() {
 		log.Fatalln(err)
 	}
 	sb := string(body)
-	log.Printf(sb)
+	//	log.Printf(sb)
+	sb2 := "This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission."
+	if strings.Contains(sb, sb2) {
+		fmt.Println(sb2)
+	}
 }
